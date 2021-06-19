07ee24f1c503baf898937fb4439b95151ba59f54
DESKTOP-KHKBE2E\bosito <bosito321@gmail.com>
2021-06-19 18:13:14 -0500

    test

--- src/context/CreateProviderQuestions.js
+++ src/context/CreateProviderQuestions.js
@@ -2,10 +2,30 @@ import React, { createContext, useContext, useEffect, useState } from 'react'
 
 const CreateAppContext = createContext();
 
-export default function CreateProviderQuestions({children}) {
-    const [test, settest] = useState(null)
+export default function CreateProviderQuestions({ children }) {
+    const [test, settest] = useState('wolas')
+
     useEffect(() => {
+        settest('wolas')
     }, [])
+
+    //const getQuestions = e => {
+        //e.preventDefault();
+        //const url = `https://opentdb.com/api.php?amount=${}&category=${}&difficulty=${}&type=${}`;
+        // fetch(url)
+        //     .then(response => {
+        //         return response.json();
+        //     })
+        //     .then(data => {
+        //         console.log(url);
+        //         console.log(data);
+        //         questions = data.results;
+        //         // nextQuestions();
+        //     })
+        //     .catch(error => {
+        //         console.log(error);
+        //     });
+    //};
 
     const context = {
         test
